8b5a6761d81fbddcbe6d27a44be5934b3ffd45de
Berat Sert <beratssert@gmail.com>
2023-05-24 00:29:36 +0300

    Add basic error handling tests

--- tests/campground.test.js
+++ tests/campground.test.js
@@ -15,6 +15,9 @@ beforeEach(async () => {
   const campground = new Campground({
     title: "first created title",
     location: "first created location",
+    price: 10,
+    description: "first description",
+    image: "image url",
   });
   await campground.save();
 });
@@ -35,6 +38,11 @@ describe("GET /campgrounds", () => {
     expect(res.text.length).toBeGreaterThan(0);
   });
 
+  it("should return an error as there is no campground", async () => {
+    const res = await request(app).get(`/campgrounds/somegarbageid`);
+    expect(res.statusCode).toBe(500);
+  });
+
   it("should return all products", async () => {
     const res = await request(app).get("/campgrounds");
     expect(res.statusCode).toBe(200);
@@ -51,10 +59,77 @@ describe("POST /campgrounds", () => {
         campground: {
           title: "new created title",
           location: "new created location",
+          price: 10,
+          description: "new description",
+          image: "image url",
         },
       });
     await Campground.findOneAndDelete({ title: "new created title" });
     expect(res.statusCode).toBe(302);
+  });
+
+  it("should return an error as title is missing", async () => {
+    const res = await request(app)
+      .post("/campgrounds")
+      .type("form")
+      .send({
+        campground: {
+          title: "",
+          location: "new created location",
+          price: 10,
+          description: "new description",
+          image: "image url",
+        },
+      });
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should return an error as description is missing", async () => {
+    const res = await request(app)
+      .post("/campgrounds")
+      .type("form")
+      .send({
+        campground: {
+          title: "new created title",
+          location: "new created location",
+          price: 10,
+          description: "",
+          image: "image url",
+        },
+      });
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should return an error as price is not a number", async () => {
+    const res = await request(app)
+      .post("/campgrounds")
+      .type("form")
+      .send({
+        campground: {
+          title: "new created title",
+          location: "new created location",
+          price: "10TL",
+          description: "new description",
+          image: "image url",
+        },
+      });
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should return an error as price is lower than 0", async () => {
+    const res = await request(app)
+      .post("/campgrounds")
+      .type("form")
+      .send({
+        campground: {
+          title: "new created title",
+          location: "new created location",
+          price: -1,
+          description: "new description",
+          image: "image url",
+        },
+      });
+    expect(res.statusCode).toBe(400);
   });
 });
 
@@ -67,11 +142,57 @@ describe("PUT /campgrounds/:campgroundId", () => {
       .put(`/campgrounds/${campground._id}`)
       .type("form")
       .send({
-        campground: { title: "updated title", location: "updated location" },
+        campground: {
+          title: "updated title",
+          location: "updated location",
+          price: 10,
+          description: "updated description",
+          image: "updated image url",
+        },
       });
 
     await Campground.findOneAndDelete({ title: "updated title" });
     expect(res.statusCode).toBe(302);
+  });
+
+  it("should return an error as missing information", async () => {
+    const campground = await Campground.findOne({
+      title: "first created title",
+    });
+    const res = await request(app)
+      .put(`/campgrounds/${campground._id}`)
+      .type("form")
+      .send({
+        campground: {
+          title: "",
+          location: "updated location",
+          price: 10,
+          description: "updated description",
+          image: "updated image url",
+        },
+      });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should return an error as price is lower than 0", async () => {
+    const campground = await Campground.findOne({
+      title: "first created title",
+    });
+    const res = await request(app)
+      .put(`/campgrounds/${campground._id}`)
+      .type("form")
+      .send({
+        campground: {
+          title: "updated title",
+          location: "updated location",
+          price: -1,
+          description: "updated description",
+          image: "updated image url",
+        },
+      });
+
+    expect(res.statusCode).toBe(400);
   });
 });
 
